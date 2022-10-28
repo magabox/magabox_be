c131851d8c9740710d4ad4f17074b45a51fe50dc
shiminki <shiminki727@gmail.com>
2022-10-28 10:12:28 +0900

    initial commit

---
+++ magabox/src/main/java/com/clone/magabox/MagaboxApplication.java
@@ -0,0 +1,13 @@
+package com.clone.magabox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagaboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagaboxApplication.class, args);
+	}
+
+}
---
+++ magabox/src/test/java/com/clone/magabox/MagaboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.magabox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagaboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
