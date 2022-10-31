9d8a688c346ca5e9ac373dcb0f3ab1d9da376aca
changwook lee <leechangwook92>
2022-10-31 10:21:24 +0900

    좋아요 수, 댓글 평점 평균, 검색, 좋아요수로 4개 조회

---
+++ magabox/src/main/java/com/clone/magabox/dto/request/MovieRequestDto.java
@@ -0,0 +1,15 @@
+package com.clone.magabox.dto.request;
+
+import org.springframework.web.multipart.MultipartFile;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
+public class MovieRequestDto {
+    private String title;
+    private String summary;
+    private MultipartFile file;
+    private int runtime;
+}
